fb5f885e690d175b57789d98779cada411500e6d
Amelie FOULQUIER <afoulqui@e2r7p1.clusters.42paris.fr>
2021-11-30 15:35:53 +0100

    modif cpp03/ex01

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: afoulqui <afoulqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 12:18:45 by afoulqui          #+#    #+#             */
-/*   Updated: 2021/08/12 18:05:17 by afoulqui         ###   ########.fr       */
+/*   Updated: 2021/11/30 15:33:53 by afoulqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int main() {
 	ClapTrap	nico("Nico");
 	ScavTrap	luis("Luis");
 
+	std::cout << std::endl;
+
 	nico.attack("Adam");
 	nico.takeDamage(3);
 	luis.attack("Adam");
@@ -25,5 +27,8 @@ int main() {
 	nico.beRepaired(3);
 	luis.beRepaired(2);
 	luis.guardGate();
+
+	std::cout << std::endl;
+
 	return (0);
 }
